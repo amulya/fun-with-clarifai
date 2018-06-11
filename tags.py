771fecad17826c0966991d13d81d8047642e0644
Amulya <amulya.mummaneni@gmail.com>
2018-06-10 21:29:46 -0400

    made img link a variable

--- tags.py
+++ tags.py
@@ -11,4 +11,6 @@ def get_relevant_tags(image_url):
 
 	return tag_urls
 n = '\n'
-print(n.join(get_relevant_tags('https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1516896485%2Fchicken-paella-ck-1803.jpg%3Fitok%3DBK5uGeSC&w=1600&q=70')))
+link = 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1516896485%2Fchicken-paella-ck-1803.jpg%3Fitok%3DBK5uGeSC&w=1600&q=70'
+
+print(n.join(get_relevant_tags(link)))
