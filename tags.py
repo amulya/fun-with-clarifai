02aaff5dbd1e70fef1d2cf8da785e9523c8dc44c
Amulya <amulya.mummaneni@gmail.com>
2018-09-02 04:52:58 -0400

    Added flask framework, upload option, stylesheet, and a couple more features

--- tags.py
+++ tags.py
@@ -1,16 +1,26 @@
+#from flask import Flask, render_template, request
 from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
 
-app = ClarifaiApp()
+myApp = ClarifaiApp()
 
 def get_relevant_tags(image_url):
-	response_data = app.tag_urls([image_url])
+
+	response_data = myApp.tag_urls([image_url])
 	
 	tag_urls = []
 	for concept in response_data['outputs'][0]['data']['concepts']:
 		tag_urls.append(concept['name'])
 
 	return tag_urls
-n = '\n'
-link = 'https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F1516896485%2Fchicken-paella-ck-1803.jpg%3Fitok%3DBK5uGeSC&w=1600&q=70'
 
-print(n.join(get_relevant_tags(link)))
+def get_relevant_tags_file(image_url):
+	
+	response = myApp.tag_files([image_url])
+
+	tag_urls = []
+	for concept in response['outputs'][0]['data']['concepts']:
+		tag_urls.append(concept['name'])
+
+	return tag_urls
+	#print(tags)
